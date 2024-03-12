29a1917e90fe5a539d66fa7c7b977fac4320aeca
Rohan Prasad Gupta <rohanprasadgupta4@gmail.com>
2024-03-12 11:12:54 +0700

    Redux work are been started
    Redux implementation work are in progress

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+
 const Username = () => {
+  const username = useSelector((state) => state.user.username);
+
+  // console.log(username);
+
+  if (!username) return null;
+
   return (
     <div className=" text-sm font-semibold hidden md:block">
-      UserName : ROHAN
+      UserName : {username}
     </div>
   );
 };
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className=" my-10 text-center">
       <h1 className=" text-center text-xl font-semibold mb-10 md:text-3xl px-4">
@@ -10,7 +14,13 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue Order {username}
+        </Button>
+      )}
     </div>
   );
 }
